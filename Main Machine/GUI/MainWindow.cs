9b3b0bf12e87df3ed4959d1da6135adc038f9311
Jenrikku <60291632+Jenrikku@users.noreply.github.com>
2022-08-31 15:56:11 +0200

    Changed to static using directives for MainWindow

--- Main Machine/GUI/MainWindow.cs
+++ Main Machine/GUI/MainWindow.cs
@@ -4,25 +4,30 @@ using NewGear.MainMachine.GUI.WindowSystem;
 using Raylib_cs;
 
 using static NewGear.MainMachine.GUI.Constants;
+using static Raylib_cs.Raylib;
+using static ImGuiNET.ImGui;
 
 namespace NewGear.MainMachine.GUI {
     internal static partial class MainWindow {
         internal static ImguiController Controller = new();
-        internal static RenderTexture2D ViewportTexture = Raylib.LoadRenderTexture(1, 1);
+        internal static RenderTexture2D ViewportTexture = LoadRenderTexture(1, 1);
         internal static bool[] OpenedWindows = new bool[3];
 
         internal static unsafe void Start() {
-            Raylib.SetTraceLogCallback(&Logging.LogConsole);
-            Raylib.SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_VSYNC_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE);
-            Raylib.InitWindow(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT, "New Gear | Alpha");
-            Raylib.SetTargetFPS(60);
+            SetTraceLogCallback(&Logging.LogConsole);
+            SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_VSYNC_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE);
+            InitWindow(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT, "New Gear | Alpha");
+            SetWindowIcon(LoadImage("icon.png"));
+            SetExitKey(KeyboardKey.KEY_NULL);
+            SetTargetFPS(60);
 
             Controller.Load(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
 
-            ImGui.GetIO().ConfigFlags |= ImGuiConfigFlags.DockingEnable;
+            GetIO().ConfigFlags |= ImGuiConfigFlags.DockingEnable;
 
-            ImGui.GetStyle().Colors[(int) ImGuiCol.WindowBg] = IMGUI_WINDOW_BACKGROUND;
-            ImGui.GetStyle().Colors[(int) ImGuiCol.DockingEmptyBg] = IMGUI_DOCKSPACE_BACKGROUND;
+            // To be replaced in the future with theming:
+            GetStyle().Colors[(int) ImGuiCol.WindowBg] = IMGUI_WINDOW_BACKGROUND;
+            GetStyle().Colors[(int) ImGuiCol.DockingEmptyBg] = IMGUI_DOCKSPACE_BACKGROUND;
 
             // Default windows (will be replaced with configuration file later)
             OpenedWindows[0] = true;  // File Tree
@@ -30,44 +35,45 @@ namespace NewGear.MainMachine.GUI {
             OpenedWindows[2] = false; // Hex Editor
 
             abortExit: // Label used for when the files have not been closed properly. (Not saved)
-            while(!Raylib.WindowShouldClose()) {
+            while(!WindowShouldClose()) {
                 // Feed the input events to our ImGui Controller, which passes them through to ImGui.
-                Controller.Update(Raylib.GetFrameTime());
+                Controller.Update(GetFrameTime());
 
                 // File drag and drop
-                if(Raylib.IsFileDropped()) {
-                    FileManager.OpenFiles(Raylib.GetDroppedFiles());
-                    Raylib.ClearDroppedFiles();
+                if(IsFileDropped()) {
+                    FileManager.OpenFiles(GetDroppedFiles());
+                    ClearDroppedFiles();
                 }
 
                 // Placeholder window (to prevent "Debug")
-                ImGui.Begin("null", 
+                Begin(string.Empty, 
                     ImGuiWindowFlags.NoInputs |
                     ImGuiWindowFlags.NoDecoration |
                     ImGuiWindowFlags.NoBackground |
                     ImGuiWindowFlags.NoFocusOnAppearing |
-                    ImGuiWindowFlags.NoBringToFrontOnFocus);
+                    ImGuiWindowFlags.NoBringToFrontOnFocus |
+                    ImGuiWindowFlags.NoSavedSettings);
 
                 if(RenderMainMenuBar())
                     break; // Go to dispose.
 
-                ImGui.End();
+                End();
 
                 #region Windows
 
-                ImGui.DockSpaceOverViewport();
+                DockSpaceOverViewport();
                 WindowManager.RenderActiveWindows();
 
                 #endregion
 
                 // Draw:
 
-                Raylib.BeginDrawing();
+                BeginDrawing();
 
-                Raylib.ClearBackground(MAIN_WINDOW_BACKGROUND);
+                ClearBackground(MAIN_WINDOW_BACKGROUND);
                 Controller.Draw();
 
-                Raylib.EndDrawing();
+                EndDrawing();
             }
 
             // Disposing:
@@ -78,10 +84,10 @@ namespace NewGear.MainMachine.GUI {
                 if(!FileManager.CloseFile(FileManager.LoadedFiles.ElementAt(i)))
                     goto abortExit; // If not all files were closed, abort exit.
 
-            Raylib.UnloadRenderTexture(ViewportTexture);
+            UnloadRenderTexture(ViewportTexture);
 
             Controller.Dispose();
-            Raylib.CloseWindow();
+            CloseWindow();
         }
     }
 }
